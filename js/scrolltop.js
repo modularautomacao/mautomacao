848bd31dd954163ccc73283023a448b94688d3fa
modularautomacao <automacao.modular@gmail.com>
2016-10-15 11:18:31 -0300

    Add files via upload

--- js/scrolltop.js
+++ js/scrolltop.js
@@ -2,12 +2,24 @@ $(document).ready(function(){
    var scroll_start = 0;
    var startchange = $('#navdef');
    var offset = startchange.offset();
+    
    $(document).scroll(function() { 
       scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
+	  
+	  var screenwidth = $(document).width();
+	  if (screenwidth < 990){
+	  	$('#navdef').css('background-color', 'rgba(0,0,0,0.7)');
+	  }
+	  
+     
+	 else{ 	 
+	 	if(scroll_start > offset.top) {
           $('#navdef').css('background-color', 'rgba(0,0,0,0.7)');
        } else {
           $('#navdef').css('background-color', 'transparent');
        }
+	 }
+	 
+	 
    });
 });
